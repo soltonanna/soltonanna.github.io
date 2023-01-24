3972b5af15d37fcad178f0ef898169d3d77641a9
Anahit Sultanova <soltonanna@gmail.com>
2023-01-24 17:26:26 +0400

    add 3 part of blog

--- src/utils/blog-items.js
+++ src/utils/blog-items.js
@@ -1,5 +1,7 @@
 import { 
     JsFundMain, JsF_0, JsF_1,  JsF_2, JsF_3, JsF_4, JsF_5, JsF_6, JsF_7, JsF_8, JsF_9, JsF_10,
+    JsBehind, BS_0, BS_1,  BS_2, BS_3, BS_4, BS_5, BS_6, BS_7, BS_8, BS_9, BS_10, BS_11,  BS_12, 
+    BS_13, BS_14, BS_15, BS_16, BS_17, BS_18, BS_19, BS_20, BS_21, BS_22, BS_23,
     OopMain, Oop_0, Oop_1,  Oop_2, Oop_3, Oop_4, Oop_5, Oop_6, Oop_7, Oop_8, Oop_9, Oop_10, 
 
 } from './media-files.js';
@@ -13,22 +15,56 @@ export const blogItems = [
         date: '24.01.2023',
         desc: "In this topic you can find some useful information about JavaScript Fundamentals. Why we use JS, short history and some basic information",
         moreDesc: `<div>
-            <p><a href=${JsF_0} target="_blank"><img src="${JsF_0}" alt="OOP screen 0"/></a></p>
-            <p><a href=${JsF_1} target="_blank"><img src=${JsF_1} alt="OOP screen 1"/></a></p>
-            <p><a href=${JsF_2} target="_blank"><img src=${JsF_2} alt="OOP screen 3"/></a></p>
-            <p><a href=${JsF_3} target="_blank"><img src=${JsF_3} alt="OOP screen 4"/></a></p>
-            <p><a href=${JsF_4} target="_blank"><img src=${JsF_4} alt="OOP screen 5"/></a></p>
-            <p><a href=${JsF_5} target="_blank"><img src=${JsF_5} alt="OOP screen 6"/></a></p>
-            <p><a href=${JsF_6} target="_blank"><img src=${JsF_6} alt="OOP screen 7"/></a></p>
-            <p><a href=${JsF_7} target="_blank"><img src=${JsF_7} alt="OOP screen 8"/></a></p>
-            <p><a href=${JsF_8} target="_blank"><img src=${JsF_8} alt="OOP screen 9"/></a></p>
-            <p><a href=${JsF_9} target="_blank"><img src=${JsF_9} alt="OOP screen 10"/></a></p>
-            <p><a href=${JsF_10} target="_blank"><img src=${JsF_10} alt="OOP screen 11"/></a></p>
+            <p><a href=${JsF_0} target="_blank"><img src="${JsF_0}" /></a></p>
+            <p><a href=${JsF_1} target="_blank"><img src=${JsF_1} /></a></p>
+            <p><a href=${JsF_2} target="_blank"><img src=${JsF_2} /></a></p>
+            <p><a href=${JsF_3} target="_blank"><img src=${JsF_3} /></a></p>
+            <p><a href=${JsF_4} target="_blank"><img src=${JsF_4} /></a></p>
+            <p><a href=${JsF_5} target="_blank"><img src=${JsF_5} /></a></p>
+            <p><a href=${JsF_6} target="_blank"><img src=${JsF_6} /></a></p>
+            <p><a href=${JsF_7} target="_blank"><img src=${JsF_7} /></a></p>
+            <p><a href=${JsF_8} target="_blank"><img src=${JsF_8} /></a></p>
+            <p><a href=${JsF_9} target="_blank"><img src=${JsF_9} /></a></p>
+            <p><a href=${JsF_10} target="_blank"><img src=${JsF_10} /></a></p>
         </div>`,
         imgUrl: JsFundMain
     },
     {
         id: `b_2`,
+        category: 'other',
+        name: 'JS Fundamentals',
+        date: '25.01.2023',
+        desc: "In this topic you can see how JavaScript work behind the scene. Know about Event Loom, Hoisting and etc.",
+        moreDesc: `<div>
+            <p><a href=${BS_0} target="_blank"><img src="${BS_0}" /></a></p>
+            <p><a href=${BS_1} target="_blank"><img src="${BS_1}" /></a></p>
+            <p><a href=${BS_2} target="_blank"><img src="${BS_2}" /></a></p>
+            <p><a href=${BS_3} target="_blank"><img src="${BS_3}" /></a></p>
+            <p><a href=${BS_4} target="_blank"><img src="${BS_4}" /></a></p>
+            <p><a href=${BS_5} target="_blank"><img src="${BS_5}" /></a></p>
+            <p><a href=${BS_6} target="_blank"><img src="${BS_6}" /></a></p>
+            <p><a href=${BS_7} target="_blank"><img src="${BS_7}" /></a></p>
+            <p><a href=${BS_8} target="_blank"><img src="${BS_8}" /></a></p>
+            <p><a href=${BS_9} target="_blank"><img src="${BS_9}" /></a></p>
+            <p><a href=${BS_10} target="_blank"><img src="${BS_10}" /></a></p>
+            <p><a href=${BS_11} target="_blank"><img src="${BS_11}" /></a></p>
+            <p><a href=${BS_12} target="_blank"><img src="${BS_12}" /></a></p>
+            <p><a href=${BS_13} target="_blank"><img src="${BS_13}" /></a></p>
+            <p><a href=${BS_14} target="_blank"><img src="${BS_14}" /></a></p>
+            <p><a href=${BS_15} target="_blank"><img src="${BS_15}" /></a></p>
+            <p><a href=${BS_16} target="_blank"><img src="${BS_16}" /></a></p>
+            <p><a href=${BS_17} target="_blank"><img src="${BS_17}" /></a></p>
+            <p><a href=${BS_18} target="_blank"><img src="${BS_18}" /></a></p>
+            <p><a href=${BS_19} target="_blank"><img src="${BS_19}" /></a></p>
+            <p><a href=${BS_20} target="_blank"><img src="${BS_20}" /></a></p>
+            <p><a href=${BS_21} target="_blank"><img src="${BS_21}" /></a></p>
+            <p><a href=${BS_22} target="_blank"><img src="${BS_22}" /></a></p>
+            <p><a href=${BS_23} target="_blank"><img src="${BS_23}" /></a></p>
+        </div>`,
+        imgUrl: JsBehind
+    },
+    {
+        id: `b_3`,
         category: 'other',
         name: 'OOP',
         date: '17.01.2023',
--- src/utils/media-files.js
+++ src/utils/media-files.js
@@ -27,6 +27,32 @@ import JsF_8 from '../media/blog/js-fundamentals/jsf_screen_8.png';
 import JsF_9 from '../media/blog/js-fundamentals/jsf_screen_9.png';
 import JsF_10 from '../media/blog/js-fundamentals/jsf_screen_10.png';
 
+import JsBehind from '../media/blog/js-behind-scene/bs_main.png';
+import BS_0 from '../media/blog/js-behind-scene/bs_screen_1.png';
+import BS_1 from '../media/blog/js-behind-scene/bs_screen_2.png';
+import BS_2 from '../media/blog/js-behind-scene/bs_screen_3.png';
+import BS_3 from '../media/blog/js-behind-scene/bs_screen_4.png';
+import BS_4 from '../media/blog/js-behind-scene/bs_screen_5.png';
+import BS_5 from '../media/blog/js-behind-scene/bs_screen_6.png';
+import BS_6 from '../media/blog/js-behind-scene/bs_screen_7.png';
+import BS_7 from '../media/blog/js-behind-scene/bs_screen_8.png';
+import BS_8 from '../media/blog/js-behind-scene/bs_screen_9.png';
+import BS_9 from '../media/blog/js-behind-scene/bs_screen_10.png';
+import BS_10 from '../media/blog/js-behind-scene/bs_screen_11.png';
+import BS_11 from '../media/blog/js-behind-scene/bs_screen_12.png';
+import BS_12 from '../media/blog/js-behind-scene/bs_screen_13.png';
+import BS_13 from '../media/blog/js-behind-scene/bs_screen_14.png';
+import BS_14 from '../media/blog/js-behind-scene/bs_screen_15.png';
+import BS_15 from '../media/blog/js-behind-scene/bs_screen_16.png';
+import BS_16 from '../media/blog/js-behind-scene/bs_screen_17.png';
+import BS_17 from '../media/blog/js-behind-scene/bs_screen_18.png';
+import BS_18 from '../media/blog/js-behind-scene/bs_screen_19.png';
+import BS_19 from '../media/blog/js-behind-scene/bs_screen_20.png';
+import BS_20 from '../media/blog/js-behind-scene/bs_screen_21.png';
+import BS_21 from '../media/blog/js-behind-scene/bs_screen_22.png';
+import BS_22 from '../media/blog/js-behind-scene/bs_screen_23.png';
+import BS_23 from '../media/blog/js-behind-scene/bs_screen_24.png';
+
 import OopMain from '../media/blog/oop/oop_main.jpg';
 import Oop_0 from '../media/blog/oop/oop_screen_0.png';
 import Oop_1 from '../media/blog/oop/oop_screen_1.png';
@@ -63,6 +89,8 @@ export {
 
     // Blogs
     JsFundMain, JsF_0, JsF_1,  JsF_2, JsF_3, JsF_4, JsF_5, JsF_6, JsF_7, JsF_8, JsF_9, JsF_10, 
+    JsBehind, BS_0, BS_1,  BS_2, BS_3, BS_4, BS_5, BS_6, BS_7, BS_8, BS_9, BS_10, BS_11,  BS_12, 
+    BS_13, BS_14, BS_15, BS_16, BS_17, BS_18, BS_19, BS_20, BS_21, BS_22, BS_23,
     OopMain, Oop_0, Oop_1, Oop_2, Oop_3, Oop_4, Oop_5, Oop_6, Oop_7, Oop_8, Oop_9, Oop_10, 
 
     // Other
