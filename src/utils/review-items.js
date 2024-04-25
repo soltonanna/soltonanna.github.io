cbb6308e4ea7378019da4715d7326e5caaf5c87f
Anahit Sultanova <soltonanna@gmail.com>
2024-04-25 10:16:35 +0400

    add review item

--- src/utils/review-items.js
+++ src/utils/review-items.js
@@ -1,5 +1,6 @@
 import VS from '../media/reviews/Vahagn.jpg';
 import AS from '../media/reviews/Ani.jpg';
+import Gayane from '../media/reviews/Gayane.jpeg'
 import NoImg from '../media/reviews/noImg.png';
 
 export const reviewItems = [
@@ -19,9 +20,16 @@ export const reviewItems = [
     },
     {
         id: `r_3`,
-        name: 'Dawm Mundo',
+        name: 'Dawm Mundo / "Brainstorm Technologies"',
         position: 'Project and Operations Management Professional',
-        review: "I’ve worked with Anahit on developing several websites using WordPress. She is a skilled developer who goes above and beyond my expectations. She delivers the product on time and in excellent quality. She will turn any UI designs into reality and is very accommodating. Each time I work with her, she exceeds my expectations. She is also an excellent English speaker and fun to work with.",
+        review: "I've worked with Anahit on developing several websites using WordPress. She is a skilled developer who goes above and beyond my expectations. She delivers the product on time and in excellent quality. She will turn any UI designs into reality and is very accommodating. Each time I work with her, she exceeds my expectations. She is also an excellent English speaker and fun to work with.",
         imgUrl: NoImg
+    },
+    {
+        id: `r_4`,
+        name: 'Gayane Kyatikyan / "VTG Software"',
+        position: 'Project and Operations Management Professional',
+        review: "For our company was a very pleasant to have you in our team for a while to develop the current project. Here are the skills you've demonstrated: a great understanding of the product, creativity and problem-solving,  attention to detail, leadership on this project, ability to work well with the team members and stakeholders, great communication skills and provide clear explanations,  dedication and hard work, consistently meets or exceeds our quality standards, a great attitude and positive energy, ability to work independently and meet deadlines, your ideas and suggestions are always insightful and valuable, work ethic, attention to user experience and user interface is impressive.",
+        imgUrl: Gayane
     }
 ]
